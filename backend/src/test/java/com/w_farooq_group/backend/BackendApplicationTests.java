0cc25bb294eb403211b584458ae545b6e6402833
Waqar <waqar-1@hotmail.co.uk>
2024-11-14 02:06:00 +0000

    chore: project setup frontend using vite and backend setup using spring initializr

---
+++ backend/src/test/java/com/w_farooq_group/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w_farooq_group.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
